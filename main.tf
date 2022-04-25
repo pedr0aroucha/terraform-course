cbc2d813959902da2e37ae7c218a92fddd5b9668
pedr0aroucha <contato.pedr0aroucha@gmail.com>
2022-04-25 18:34:37 -0300

    feat: create dev7

--- main.tf
+++ main.tf
@@ -93,3 +93,16 @@ resource "aws_dynamodb_table" "dynamodb-homologacao" {
     type = "S"
   }
 }
+
+resource "aws_instance" "dev7" {
+  provider      = aws.us-east-2
+  ami           = var.amis["us-east-2"]
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  tags = {
+    Name = "dev7"
+  }
+
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
+}
