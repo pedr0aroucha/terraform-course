2ad5a2faf5a2d47774318e4e88ea17e1b26987f6
pedr0aroucha <contato.pedr0aroucha@gmail.com>
2022-04-25 17:23:06 -0300

    feat: Deploy da infraestrutura multi-região

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+provider "aws" {
+  alias   = "us-east-2"
+  version = "~> 2.0"
+  region  = "us-east-2"
+}
+
 resource "aws_instance" "dev" {
   count         = 3
   ami           = "ami-04505e74c0741db8d"
@@ -16,24 +22,8 @@ resource "aws_instance" "dev" {
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}", "sg-0cbce4d4050554cc9"]
 }
 
-resource "aws_security_group" "acesso-ssh" {
-  name        = "acesso-ssh"
-  description = "Acesso SSH"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ssh"
-  }
-}
-
 resource "aws_s3_bucket" "dev4" {
-  bucket = "dev4"
+  bucket = "pedr0arouca-dev4"
   acl    = "private"
 
   tags = {
@@ -51,6 +41,10 @@ resource "aws_instance" "dev4" {
   }
 
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}", "sg-0cbce4d4050554cc9"]
+
+  depends_on = [
+    aws_s3_bucket.dev4
+  ]
 }
 
 resource "aws_instance" "dev5" {
@@ -63,4 +57,17 @@ resource "aws_instance" "dev5" {
   }
 
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}", "sg-0cbce4d4050554cc9"]
+}
+
+resource "aws_instance" "dev6" {
+  provider      = aws.us-east-2
+  ami           = "ami-0c6a6b0e75b2b6ce7"
+  instance_type = "t2.micro"
+  key_name      = "terraform-aws"
+
+  tags = {
+    Name = "dev6"
+  }
+
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}", "sg-0cbce4d4050554cc9"]
 }
