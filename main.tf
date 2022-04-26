4b52dfe2e984e772a2628424776cec7b4755b238
pedr0aroucha <contato.pedr0aroucha@gmail.com>
2022-04-26 12:06:41 -0300

    chore: remove aws provider us east 2

--- main.tf
+++ main.tf
@@ -3,12 +3,6 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-provider "aws" {
-  alias   = "us-east-2"
-  version = "~> 2.0"
-  region  = "us-east-2"
-}
-
 resource "aws_instance" "dev" {
   count         = 3
   ami           = var.amis["us-east-1"]
@@ -20,4 +14,13 @@ resource "aws_instance" "dev" {
   }
 
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_s3_bucket" "bucket-pedroaroucha" {
+  bucket = "bucket-pedroaroucha"
+  acl    = "private"
+
+  tags = {
+    Name = "bucket-pedroaroucha"
+  }
 }
